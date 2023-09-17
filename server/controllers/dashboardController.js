0801898855ce0ab4279ee197cfe6eec027322bb3
iren <irasibil2@gmail.com>
2023-09-17 11:06:38 +0300

    added view/update/delete note, pagination

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,16 +1,97 @@
+const Note = require('../models/Notes');
+const mongoose = require('mongoose');
+
 /**
  * GET /
  * Dashboard
  */
 
 exports.dashboard = async (req, res) => {
+    let perPage = 12;
+    let page = req.query.page || 1;
+
     const locals = {
         title: "Dashboard",
         description: "Free NodeJs Notes App"
     }
 
-    res.render('dashboard/index', {
-        locals,
-        layout: '../views/layouts/dashboard'
-    })
+    try {
+        const notes = await Note.aggregate([
+            { $sort: { updatedAt: -1 } },
+            { $match: { user: new mongoose.Types.ObjectId(req.user.id) } },
+            {
+                $project: {
+                    title: { $substr: [ '$title', 0, 30  ] },
+                    body: { $substr: [ '$body', 0, 100  ] }
+                }
+            }
+        ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Note.count();
+
+        res.render('dashboard/index', {
+            userName: req.user.firstName,
+            locals,
+            notes,
+            layout: "../views/layouts/dashboard",
+            current: page,
+            pages: Math.ceil(count / perPage)
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+/**
+ * GET /
+ * View Specific Note
+ */
+exports.dashboardViewNote = async (req, res) => {
+    const note = await Note.findById({ _id: req.params.id })
+        .where({ user: req.user.id }).lean();
+
+    if (note) {
+        res.render('dashboard/view-note', {
+            noteID: req.params.id,
+            note,
+            layout: '../views/layouts/dashboard'
+        })
+    } else {
+        res.send("Something went wrong.")
+    }
+}
+
+/**
+ * PUT /
+ * Update Specific Note
+ */
+exports.dashboardUpdateNote = async (req, res) => {
+    try {
+        await Note.findOneAndUpdate(
+            { _id: req.params.id },
+            { title: req.body.title, body: req.body.body }
+        ).where({ user: req.user.id });
+
+        res.redirect('/dashboard');
+    } catch (error) {
+        console.log(erro);
+    }
+}
+
+/**
+ * DELETE /
+ * Delete Note
+ */
+exports.dashboardDeleteNote = async (req, res) => {
+    try {
+        await Note.deleteOne({ _id: req.params.id }).where({ user: req.user.id });
+        res.redirect('/dashboard');
+    } catch (error) {
+        console.log(error);
+    }
 }
